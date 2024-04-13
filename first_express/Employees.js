08083c54dc13f1059f110263e99a484387b3e470
jerripat <jerripatterson@gmail.com>
2024-04-12 20:30:47 -0500

    added index.js as well as an api

---
+++ first_express/Employees.js
@@ -0,0 +1,25 @@
+const emoloyees = [
+
+    {
+        name: 'John',
+        email: "john@<EMAIL>",
+        age: 30,
+    },
+    {
+        name: 'Jane',
+        email: "jane@<EMAIL>",
+        age: 25,
+    },
+    {
+        name: 'Peter',
+        email: "peter@<EMAIL>",
+        age: 35,
+    },
+    {
+        name: 'Mary',
+        email: "mary@<EMAIL>",
+        age: 20,
+    },
+
+]
+module.exports = emoloyees;
